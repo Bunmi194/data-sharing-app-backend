bfc56624d529633a9b70ecee2b87ed58083a15a8
Bunmi Oladipupo <bunmidavids194@gmail.com>
2023-08-28 15:22:34 +0100

    Fix build

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase-admin/auth";
 import path from "path";
 
 export const auth = getAuth;
-const serviceAccountDirectory = path.join(__dirname, "./service-account");
+const serviceAccountDirectory = path.join(__dirname, "./service-account.json");
 const { credentials } = require(serviceAccountDirectory);
 
 admin.initializeApp({
